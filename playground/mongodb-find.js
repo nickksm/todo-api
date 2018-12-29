b5cb21ec76ed19ca77826cce2d10b318e290b14c
Nick <nick.kusumo@gmail.com>
2018-12-30 02:07:32 +0700

    first commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,18 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+      return console.log('Unable to connect to MongoDB Server')
+    }
+
+    console.log('Connection Successful')
+
+    db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+        console.log('Todos')
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    })
+    
+    //db.close();
+});
